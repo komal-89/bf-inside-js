202e260ac81a226f072372eae45c3a56446ba9bc
komal-89 <kp30512@gmail.com>
2023-06-14 10:51:10 +0200

    git commit

--- lesson-plans/functions-and-unit-testing/1-functions/exercises/0-example-scramble.js
+++ lesson-plans/functions-and-unit-testing/1-functions/exercises/0-example-scramble.js
@@ -6,22 +6,28 @@ const scramble = (param1, param2, param3) => {
   return result;
 };
 
-const _1_expected = 'zyx';
-const _1_actual = scramble('x', 'z', 'y');
-console.assert(_1_actual === _1_expected, 'Test 1');
+const _1_actual = scramble('a', 'c', 'b');
+const _1_expect = _;
+console.assert(_1_actual === _1_expect, 'Test 1');
 
-const _2_expected = 'yzx';
-const _2_actual = scramble('x', 'y', 'z');
-console.assert(_2_actual === _2_expected, 'Test 2');
+const _2_expect = _;
+const _2_actual = scramble('a', 'b', 'c');
+console.assert(_2_actual === _2_expect, 'Test 2');
 
-// correct the expected value to pass the assertion
-const _3_expected = 'yzx';
-const _3_actual = scramble('z', 'y', 'x');
-console.assert(_3_actual === _3_expected, 'Test 3');
+const _3_expect = _;
+const _3_actual = scramble('c', 'b', 'a');
+console.assert(_3_actual === _3_expect, 'Test 3');
 
-// correct the arguments to pass the assertion
-const _4_expected = 'zyx';
-const _4_actual = scramble('y', 'x', 'z');
-console.assert(_4_actual === _4_expected, 'Test 4');
+const _4_expect = _;
+const _4_actual = scramble('b', 'a', 'c');
+console.assert(_4_actual === _4_expect, 'Test 4');
+
+const _5_expect = _;
+const _5_actual = scramble('b', 'c', 'a');
+console.assert(_5_actual === _5_expect, 'Test 5');
+
+const _6_expect = _;
+const _6_actual = scramble('c', 'a', 'b');
+console.assert(_6_actual === _6_expect, 'Test 6');
 
 console.log('-- end --');
